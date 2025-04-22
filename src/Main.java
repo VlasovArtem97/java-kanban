c75a5a7e9348dc33871deebdeb29864406b76fef
VlasovArtem <Yukionn@yandex.ru>
2025-04-22 17:50:39 +0300

    20. Добавлен сервер к трекеру задач (прохождение автотестов №1

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,6 @@ import tasktracker.TaskManager;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -23,29 +22,29 @@ public class Main {
             File file = Paths.get(LOCATION).toFile();
             TaskManager taskManager = Managers.getDefault(file);
 
-//            Task task1 = new Task("Задача Task 1", "Описание задачи Task 1", Status.NEW,
-//                    LocalDateTime.of(2024, 3, 29, 12, 0), Duration.ofHours(1));
-//
-//            Epic epic1 = new Epic("Epic 1", "Описание 1 Epic задачи");
-//            Epic epic2 = new Epic("Epic 2", "Описание 2 Epic задачи");
-//
-//            taskManager.addTask(task1);
-//            taskManager.addEpic(epic1);
-//            taskManager.addEpic(epic2);
-//
-//            SubTask subTask1 = new SubTask("SubTask 1", "Описание subTask 1", epic1.getId(),
-//                    LocalDateTime.of(2024, 3, 29, 13, 0), Duration.ofHours(1));
-//            SubTask subTask2 = new SubTask("SubTask 2", "Описание subTask 2", epic1.getId(),
-//                    LocalDateTime.of(2024, 3, 29, 14, 0), Duration.ofHours(1));
-//            SubTask subTask3 = new SubTask("SubTask 3", "Описание subTask 3", epic1.getId(),
-//                    LocalDateTime.of(2024, 3, 29, 15, 0), Duration.ofHours(1));
-//
-//            taskManager.addSubTask(subTask1);
-//            taskManager.addSubTask(subTask2);
-//            taskManager.addSubTask(subTask3);
-//
-//
-//            printAllTasks(taskManager);
+            Task task1 = new Task("Задача Task 1", "Описание задачи Task 1", Status.NEW,
+                    LocalDateTime.of(2024, 3, 29, 12, 0), Duration.ofHours(1));
+
+            Epic epic1 = new Epic("Epic 1", "Описание 1 Epic задачи");
+            Epic epic2 = new Epic("Epic 2", "Описание 2 Epic задачи");
+
+            taskManager.addTask(task1);
+            taskManager.addEpic(epic1);
+            taskManager.addEpic(epic2);
+
+            SubTask subTask1 = new SubTask("SubTask 1", "Описание subTask 1", epic1.getId(),
+                    LocalDateTime.of(2024, 3, 29, 13, 0), Duration.ofHours(1));
+            SubTask subTask2 = new SubTask("SubTask 2", "Описание subTask 2", epic1.getId(),
+                    LocalDateTime.of(2024, 3, 29, 14, 0), Duration.ofHours(1));
+            SubTask subTask3 = new SubTask("SubTask 3", "Описание subTask 3", epic1.getId(),
+                    LocalDateTime.of(2024, 3, 29, 15, 0), Duration.ofHours(1));
+
+            taskManager.addSubTask(subTask1);
+            taskManager.addSubTask(subTask2);
+            taskManager.addSubTask(subTask3);
+
+
+            printAllTasks(taskManager);
 
             System.out.println(taskManager.getPrioritizedTasks());
 
